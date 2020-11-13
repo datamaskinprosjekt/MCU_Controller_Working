aa10e3a2d1cee6a38bb1391f5fd01e6b6b76f07d
unknown <medlem@incomplete.samba.omegav.no>
2020-11-13 22:55:20 +0100

    Initial commit, and it's working

---
+++ madnes_controller/main.c
@@ -0,0 +1,90 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define DDR_SPI DDRB
+#define DD_MISO DDB4
+
+#define INPUT_PIN PINC
+#define INPUT_PORT PORTC
+#define INPUT_DDR DDRC
+#define SWA PC4
+#define SWB PC3
+#define SWX PC2
+#define SW_BITMASK ((1 << SWA) | (1 << SWB) | (1 << SWX))
+
+#define REFS_BM 0b11000000
+
+#define ADMUX_BM 0b00001111
+#define ADMUX_HORIZONTAL 0b0000
+#define ADMUX_VERTICAL   0b0001
+
+void setup_spi () {
+	DDR_SPI |= (1 << DD_MISO);
+	SPCR |= (1 << SPE);
+}
+
+void setup_input () {
+	INPUT_DDR &= ~SW_BITMASK;
+	INPUT_PORT |= SW_BITMASK;
+}
+
+void setup_adc () {
+	// Disable power saving
+	PRR &= 1 << PRADC;
+	
+	// Set VCC voltage ref
+	ADMUX &= ~REFS_BM;
+	ADMUX |= (1 << REFS0);
+	
+	// Fill high byte register with 8 highest bits (needed for 8-bit resolution read)
+	ADMUX |= (1 << ADLAR);
+	
+	// Enable ADC
+	ADCSRA |= (1 << ADEN);
+}
+
+uint8_t read_adc(uint8_t adc_mux) {
+	ADMUX = (ADMUX & ~ADMUX_BM) | adc_mux; // select input
+	ADCSRA |= (1 << ADSC);                 // start conversion
+	while (ADCSRA & (1 << ADSC));          // wait until finished
+	return ADCH;
+}
+
+// Read the three buttons and return them in one uint8_t,
+// where A, B, X are given by bit 0, 1, 2. Active high.
+uint8_t read_buttons () {
+	return
+		((~INPUT_PIN & (1 << SWA)) >> (SWA-0)) |
+		((~INPUT_PIN & (1 << SWB)) >> (SWB-1)) |
+		((~INPUT_PIN & (1 << SWX)) >> (SWX-2)) 
+	;
+}
+
+void wait_for_spi () {
+	while (~SPSR & (1 << SPIF));
+}
+
+int main(void) {
+	setup_spi();
+	setup_adc();
+	setup_input();
+	while (1) {
+		wait_for_spi();
+		switch (SPDR) {
+		case 0:
+			SPDR = read_buttons();
+			break;
+		case 1:
+			SPDR = read_adc(ADMUX_HORIZONTAL);
+			break;
+		case 2:
+			SPDR = read_adc(ADMUX_VERTICAL);
+			break;
+		default:
+			break;
+		}
+	}
+}
